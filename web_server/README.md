97def940537cc6358fac63c674cb61079ac16347
Elvis <dmanzimul@alustudent.com>
2026-07-21 10:11:05 +0200

    Add load balancer project files

--- web_server/README.md
+++ web_server/README.md
@@ -1,1 +1,1 @@
-do not read me
+# Web Server Project
--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Fix WordPress 500 error caused by .phpp typo in wp-settings.php
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
